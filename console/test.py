e79cc5379911ee746e9b83a5c1f6b99eedbcd58a
chaogao <chaogao2016@outlook.com>
2018-02-13 17:19:02 +0800

    测试

--- console/test.py
+++ console/test.py
@@ -1,5 +1,28 @@
 import sys
-import preInit
+import requests
 
-print(sys.path)
+# 定义常量
+hacgUrl = "http://www.hacg.love/wp/category/all/anime/"
+
+# 从页面中解析出单条item信息
+def getItemInfo(htmlStr):
+    pass
+
+# 将item数据存入到数据库或文件中
+def saveItem():
+    pass
+
+# 遍历所有分页，提取中其中的item
+pageIndex = 1
+
+while True:
+    pageUrl = hacgUrl + ("page/%d/" % (pageIndex))
+
+    r = requests.get(pageUrl,timeout=20)
+
+    item = getItemInfo(r.content)
+
+    print(pageIndex)
+
+    pageIndex = pageIndex + 1
 
