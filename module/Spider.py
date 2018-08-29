08152a65c76cf3a51e754cbf90aabaa4297fbe69
gaochao <gaochao03@meicai.cn>
2018-08-29 18:14:06 +0800

    还没找到解决动态页面的方法

--- module/Spider.py
+++ module/Spider.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 import time
 import regex as re
-from selenium import webdriver
 import requests
 import config.constant as constant
-
+import module.browser.Chrome as Chrome
+from module.browser.Chrome import Chrome
 
 __author__ = 'gaochao'
 
@@ -42,9 +42,9 @@ class Spider(object):
         if self.animationUrl.strip() == '' or self.gameUrl.strip() == '' or self.comicUrl.strip() == '':
             print("开始抓取首页链接")
             # 未抓取过首页数据
-            home_content = requests.get(constant.SITE_URL)
+            home_content = Chrome.instance().get_home_full_document(constant.SITE_URL)
             regex = re.compile("(?<=top.*href=\").{4,}(?=/\")")
-            result = regex.findall(home_content.text)
+            result = regex.findall(home_content)
 
             for uri in result :
                 if uri.find("game") != -1 :
@@ -73,10 +73,10 @@ class Spider(object):
             if self.animationPageSize == 0:
                 print("动画页第一次抓取，先初始化分页数据")
                 first_animation_url = self.animationUrl + 'page/' + str(self.curAnimationPage)
-                first_animation_page_content = requests.get(first_animation_url)
+                first_animation_page_content = Chrome.instance().get_animation_full_document(first_animation_url)
                 # 抓取动画页最后一页id
                 regex_last_page = re.compile("(?<=['\"]wp-pagenavi['\"][\s\S]*last.*page/)\d*(?=/?)")
-                result_last_page = regex_last_page.findall(first_animation_page_content.text)
+                result_last_page = regex_last_page.findall(first_animation_page_content)
                 print(result_last_page)
                 result_last_page = [88]
                 if len(result_last_page) < 1 or int(result_last_page[0]) <= 0 :
@@ -90,17 +90,16 @@ class Spider(object):
             for page_id in range(1,self.animationPageSize + 1) :
                 print('开始第' + str(self.curAnimationPage) + '页动画页数据抓取')
                 current_animation_url = self.animationUrl + 'page/' + str(self.curAnimationPage)
-                animation_page_content = requests.get(current_animation_url)
+                animation_page_content = Chrome.instance().get_animation_full_document(current_animation_url)
                 # 抓取当前页面子链接正则
                 regex_sub_link = re.compile("(?<=article[\s\S]*entry-title.*=\").*(?=\" )")
-                result_sub_link = regex_sub_link.findall(animation_page_content.text)
-                # print("===========")
-                # f = open(constant.RUNTIME_PATH + '/test.txt', 'w')
-                # f.write(animation_page_content.text)
-                # f.close()
-                # print(animation_page_content.text)
-                # print("============")
-                print(result_sub_link)
+                result_sub_link = regex_sub_link.findall(animation_page_content)
+
+                print("===========")
+                f = open(constant.RUNTIME_PATH + '/test.log', 'w')
+                f.write(animation_page_content)
+                f.close()
+                print("============")
                 for sub_link in result_sub_link :
                     print(sub_link)
                 self.curAnimationPage += 1
@@ -109,40 +108,16 @@ class Spider(object):
 
     # 程序启动
     def run(self):
-        # from selenium import webdriver
-        # from selenium.webdriver.chrome.options import Options
 
-        # options = Options()
-        # options.add_argument("--headless")  # Runs Chrome in headless mode.
-        # # options.add_argument('--no-sandbox')  # # Bypass OS security model
-        # options.add_argument('start-maximized')
-        # options.add_argument('disable-infobars')
-        # options.add_argument("--disable-extensions")
-        #
-        # options = webdriver.ChromeOptions()
-        # options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-        # chrome_driver_binary = "/usr/local/bin/chromedriver"
-        # driver = webdriver.Chrome(chrome_driver_binary, chrome_options=options)
-        # driver = webdriver.Chrome( executable_path='/data/apps/chromedriver/chromedriver',chrome_options=options)
-        # driver.get("http://baidu.com")
-        # title = driver.title
-        # print(title)
+        while self.check_status() :
+            self.home_action()
 
-        from selenium import webdriver
+            self.animation_action()
 
-        options = webdriver.ChromeOptions()
-        options.add_argument("--headless")
-        chrome_driver_binary = "/usr/local/bin/chromedriver"
-        driver = webdriver.Chrome(chrome_options=options)
+            print('大循环终止三秒')
+            time.sleep(constant.SLEEP_DELAY)
 
-        # while self.check_status() :
-        #     self.home_action()
-        #
-        #     self.animation_action()
-        #
-        #     print('大循环终止三秒')
-        #     time.sleep(constant.SLEEP_DELAY)
-
+        Chrome.instance().close_browser()
         print("程序终止")
         exit()
 
